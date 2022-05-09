8ed3dd91bcd6c14b51b52a1e9a81f162ecec33ff
Carol Ñavincopa <cnavinco@nttdata.com>
2022-05-09 18:21:09 -0500

    primer commit

---
+++ src/test/java/com/nttdata/api/typeoperation/ApiTypeoperationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.api.typeoperation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTypeoperationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
